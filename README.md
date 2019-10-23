81594f6f18396a170981c57e8703eebe148ab0cf
Kravs <a.kravs@gmail.com>
2019-10-23 17:58:54 +0300

    temporary jitpack conf

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # smart-on-fhir
+
+As a temporary solution you might use JitPack:
+[![](https://jitpack.io/v/HealthLX/smart-on-fhir.svg)](https://jitpack.io/#HealthLX/smart-on-fhir)
--- core/src/test/java/com/healthlx/smartonfhir/core/CustomAuthorizationRequestResolverTest.java
+++ core/src/test/java/com/healthlx/smartonfhir/core/CustomAuthorizationRequestResolverTest.java
@@ -4,15 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 
 import java.util.Collections;
-import java.util.stream.Stream;
 
 class CustomAuthorizationRequestResolverTest {
 
-    @Test
-    void lambdaExpressions() {
-        DefaultSavedRequest savedRequest = new DefaultSavedRequest.Builder()
-                .setParameters(Collections.singletonMap("launch", new String[]{"SOME_CODE_1"}))
-                .build();
-        //bla bla bla consider split logic from spring flow integration
-    }
+  @Test
+  void lambdaExpressions() {
+    DefaultSavedRequest savedRequest = new DefaultSavedRequest.Builder().setParameters(
+        Collections.singletonMap("launch", new String[] {"SOME_CODE_1"}))
+        .build();
+    //bla bla bla consider split logic from spring flow integration
+  }
 }
