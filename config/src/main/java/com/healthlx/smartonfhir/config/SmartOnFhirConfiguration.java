3d726944f415aa8e7ec974a08cb9cf13b55abafb
a.kravs <a.kravs@gmail.com>
2019-10-24 12:37:56 +0300

    fixed build

--- config/src/main/java/com/healthlx/smartonfhir/config/SmartOnFhirConfiguration.java
+++ config/src/main/java/com/healthlx/smartonfhir/config/SmartOnFhirConfiguration.java
@@ -1,6 +1,11 @@
 package com.healthlx.smartonfhir.config;
 
+import com.healthlx.smartonfhir.core.SmartOnFhirAccessTokenResponseClient;
+import com.healthlx.smartonfhir.core.SmartOnFhirContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpSession;
 
 @Configuration
 public class SmartOnFhirConfiguration {
