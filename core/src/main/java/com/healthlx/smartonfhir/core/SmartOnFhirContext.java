a53985235027b67a5d8bf18ef014626e5453316d
a.kravs <a.kravs@gmail.com>
2019-11-06 16:48:31 +0200

    get token implemented

--- core/src/main/java/com/healthlx/smartonfhir/core/SmartOnFhirContext.java
+++ core/src/main/java/com/healthlx/smartonfhir/core/SmartOnFhirContext.java
@@ -2,6 +2,9 @@ package com.healthlx.smartonfhir.core;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.util.Assert;
 
@@ -17,9 +20,11 @@ public class SmartOnFhirContext {
   static final String SMART_CONTEXT_SESSION_KEY = "SMART_ON_FHIR_CONTEXT";
 
   private HttpSession session;
+  private OAuth2AuthorizedClientService authorizedClientService;
 
-  public SmartOnFhirContext(HttpSession session) {
+  public SmartOnFhirContext(HttpSession session, OAuth2AuthorizedClientService authorizedClientService) {
     this.session = session;
+    this.authorizedClientService = authorizedClientService;
   }
 
   public String getPatient() {
@@ -37,13 +42,21 @@ public class SmartOnFhirContext {
     return ((OAuth2User) authentication.getPrincipal()).getAttribute("profile");
   }
 
-  void setContextData(Map<String, Object> additionalParameters) {
-    session.setAttribute(SMART_CONTEXT_SESSION_KEY, additionalParameters);
-  }
-
   public String getContextValue(String contextKey) {
     Map attribute = (Map) session.getAttribute(SMART_CONTEXT_SESSION_KEY);
     Assert.notNull(attribute, "SMART on FHIR context is not initialized. Check authorization.");
     return (String) attribute.get(contextKey);
   }
+
+  public String getToken(String clientRegistrationId){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    OAuth2AuthorizedClient authorizedClient = this.authorizedClientService.loadAuthorizedClient(clientRegistrationId,authentication.getName());
+    OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+    return accessToken.getTokenValue();
+  }
+
+  void setContextData(Map<String, Object> additionalParameters) {
+    session.setAttribute(SMART_CONTEXT_SESSION_KEY, additionalParameters);
+  }
+
 }
