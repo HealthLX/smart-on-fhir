b5ba250c4002be9187ed46dc0780407409a3fe64
a.kravs <a.kravs@gmail.com>
2019-11-07 13:47:11 +0200

    jacoco codacy reporting added

---
+++ config/src/test/java/com/healthlx/smartonfhir/config/SmartOnFhirConfigurationTest.java
@@ -0,0 +1,14 @@
+package com.healthlx.smartonfhir.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmartOnFhirConfigurationTest {
+
+  @Test
+  void getSmartLaunchParameters() {
+    assertEquals("22", "22" );//dumb test todo remove
+  }
+
+}
