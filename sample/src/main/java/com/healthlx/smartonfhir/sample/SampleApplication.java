440d48c3b9bee2049788bfb2be3d5ee5a7a462c0
Kravs <a.kravs@gmail.com>
2019-11-28 17:56:14 +0200

    sample skeleton added

---
+++ sample/src/main/java/com/healthlx/smartonfhir/sample/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.healthlx.smartonfhir.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleApplication.class, args);
+  }
+
+}
